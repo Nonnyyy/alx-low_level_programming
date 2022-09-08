76ac720ab6c3e6567dd2300a3b743e7e4552cd53
Nonnyyyy <migwilg025@gmail.com>
2022-09-08 15:31:27 -0700

    Unix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,14 +3,14 @@
 /**
  *
  * main - Entry point
- *  Return: Always 1 (Success)
+ * Return: 1
  */
 
 int main(void)
+	void write();
 
 {
-
-		write(2, "and that piece of art is useful- Dora Korpar, 2015-10-19\n", 61);
+	write(2, "and that piece of art is useful- Dora Korpar, 2015-10-19\n", 61);
 
 			return (1);
 
