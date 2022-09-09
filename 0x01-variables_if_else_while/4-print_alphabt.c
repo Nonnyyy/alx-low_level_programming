853f029992c173c9293660f477e2d2d83e7f5872
Nonnyyyy <migwilg025@gmail.com>
2022-09-09 07:52:02 -0700

    Print alphabet

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main- This is the entry point
+ *
+ * Return: 0
+ */
+int main(void)
+
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+		if (alphabet == 'q')
+			continue;
+
+		else if (alphabet == 'e')
+			continue;
+
+		putchar (alphabet);
+	}
+	
+	pucharc('\n');
+
+	return (0);
+}
