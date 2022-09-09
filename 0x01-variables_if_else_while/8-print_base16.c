cf0a45ca8d3baf8c60ed7a19cb50dc303127e18f
Nonnyyyy <migwilg025@gmail.com>
2022-09-09 08:24:55 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main- This is the entry point
+ *
+ * Return: 0
+ */
+int main(void)
+
+{
+	char base_16;
+
+	for (base_16 = 48; base_16 <= 57; base_16++)
+	{
+		putchar(base_16);
+	}
+	putchar('\n');
+
+	return (0);
+}
