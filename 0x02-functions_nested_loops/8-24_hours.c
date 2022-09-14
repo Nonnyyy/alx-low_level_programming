d03a5d52252c23342e94157aebd74c61d52eba3b
Nonnyyyy <migwilg025@gmail.com>
2022-09-14 11:36:11 -0700

    Hrs, min

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -4,7 +4,7 @@
  */
 void jack_bauer(void)
 {
-	int min,hr;
+	int min, hr;
 
 	for (hr = 0; hr <= 23; hr++)
 	{
@@ -12,7 +12,7 @@ void jack_bauer(void)
 		{
 			_putchar((hr / 10) + 48);
 			_putchar((hr % 10) + 48);
-			_putchar('.');
+			_putchar(':');
 			_putchar((min / 10) + 48);
 			_putchar((min % 10) + 48);
 			_putchar('\n');
