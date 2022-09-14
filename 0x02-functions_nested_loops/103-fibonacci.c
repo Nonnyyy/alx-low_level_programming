88cb754e55cf2f6cdf7fcc3915b1e0bb60c12bd0
Nonnyyyy <migwilg025@gmail.com>
2022-09-14 14:13:53 -0700

    103

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,8 +1,7 @@
 #include "main.h"
 /**
  * main -this is the entry point
- * Description: prints the sum of even valued fibonacci sequence not exceeding 
- * 4 million
+ * Description:Prints sum of even-valued fibonacci sequence not exceed 4 million
  * Return: 0
  */
 int main(void)
