b3871306683ac79e673961de7464e0d947cfff68
Nonnyyyy <migwilg025@gmail.com>
2022-09-14 05:05:03 -0700

    Putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -6,8 +6,7 @@
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
-int _islower(int c);
-int isalpha(int c);
+int _islower(int c);;
 int _isalpha(int c);
 int print_sign(int n);
 int  _abs(int);
@@ -17,4 +16,6 @@ void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
+
+#endif /* MAIN_H */
 
