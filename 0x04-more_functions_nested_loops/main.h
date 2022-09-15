e5550cef810e6d90019c8d3d5beb76070a124ffc
Nonnyyyy <migwilg025@gmail.com>
2022-09-15 05:13:16 -0700

    multiply

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,14 +2,14 @@
 #define MAIN_H
 
 /**
- * main- header file for prototypes
- * void prototypes (void)
- * int prototypes (int)
+ * main- header file for protoypes
+ * void protoypes (void)
+ * int protoypes (int)
  */
-int _putchar(char c);
-int _isupper(int c);
-int _isdigit(int c);
-int mul(int a, int b)
+int _putchar(char);
+int _isupper(int);
+int _isdigit(int);
+int mul(int, int);
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
@@ -17,6 +17,7 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
+void print_number(int n);
 
 #endif /* MAIN_H */
 
