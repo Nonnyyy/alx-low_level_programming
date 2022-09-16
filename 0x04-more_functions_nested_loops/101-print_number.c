3254fc99cea9b499ac1e3b0951c2c2cd9a0599a1
Nonnyyyy <migwilg025@gmail.com>
2022-09-16 04:40:44 -0700

    Prime numbers

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -9,15 +9,16 @@
 
 void print_number(int n)
 {
-	unsigned int x;
+	unsigned int num = n;
 
-	x = n;
 	if (n < 0)
 	{
-		_putchar(45);
-		x = -n
+		putchar('-');
+		num = -num;
 	}
-	if (x / 10)
-		print_number(x / 10);
-	_putchar((x % 10) + '0');
+	if (num > 9)
+	{
+		print_number(num / 10);
+	}
+	putchar((num % 10) + '0');
 }
