01ade2c6d8bfc3b1e53c95344f405b357e881d17
Nonnyyyy <migwilg025@gmail.com>
2022-09-19 12:50:53 -0700

    101-keygen.c

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "main.h"
+
+/**
+ * main- random password for 101-crackme
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+	int x;
+
+	srand(time(0));
+	while (x <=2645)
+	{
+		c = rand() % 128;
+		x += c;
+		putchar(c);
+	}
+	putchar(2772 - x);
+
+	return (0);
+}
