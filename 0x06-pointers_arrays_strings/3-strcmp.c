852f95e064e3b43ccf09a2d4940caef9a52ccae9
Nonnyyyy <migwilg025@gmail.com>
2022-09-22 05:57:06 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -13,15 +13,20 @@
 int _strcmp(char *s1, char *s2)
 {
 
-	int i = 0, diff - 0;
+	int i = 0, diff = 0;
 	
 	while (1)
 	{
 		if (s1[i] == '\0' && s2[i] == '\0')
-			btrsk;
+			break;
 		else if (s1[i] == '\0')
 		{
 			diff = s2[i];
+			break;
+		}
+		else if (s2[i] == '\0')
+		{
+			diff = s1[i];
 			break;
 		}
 		else if (s1[i] != s2[i])
