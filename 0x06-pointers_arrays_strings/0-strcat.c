66c54610e27ffde4427d723f1d5673ea87998487
Nonnyyyy <migwilg025@gmail.com>
2022-09-22 05:11:06 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,4 +1,4 @@
-#includem"main.h"
+#include "main.h"
 
 /**
  * _strcat - it connects the string pointed to by @src to
@@ -12,24 +12,13 @@
 char *_strcat(char *dest, char *src)
 {
 
-	int i;
-	int j;
+	int index = 0, dest_len = 0;
 
-	i = 0;
-	j = 0;
+	while (dest[index++])
+		dest_len++;
 
-	while (dest[i] != '\0')
-	{
-		i++
-	}
-	while (src[j] != '\0')
-	{
-		dest[i] = src[j];
-		j++;
-		i++;
-	}
-
-	dest[i] = '\0';
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
 
 	return (dest);
 }
